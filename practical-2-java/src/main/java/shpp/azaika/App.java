4f32c1eb85aaae2a0caa5431d64c5e11bf2b96aa
Andrew Zaika <lytiy7404@gmail.com>
2024-11-29 00:57:31 +0200

    added git ignore

--- practical-2-java/src/main/java/shpp/azaika/App.java
+++ practical-2-java/src/main/java/shpp/azaika/App.java
@@ -5,6 +5,6 @@ package shpp.azaika;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
