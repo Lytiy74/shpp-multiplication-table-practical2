3ad82a8b5e4e5513ad622d6ca0261b1252acbc1a
Andrew Zaika <lytiy7404@gmail.com>
2024-11-30 15:11:28 +0200

    Changed path to properties, now it takes from resources

--- practical-2-java/src/main/java/shpp/azaika/util/Config.java
+++ practical-2-java/src/main/java/shpp/azaika/util/Config.java
@@ -1,6 +1,5 @@
 package shpp.azaika.util;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +9,7 @@ public class Config {
     private static Properties properties;
 
     static {
-        try(InputStream inputStream = new FileInputStream("config.properties")) {
+        try(InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties")) {
             properties = new Properties();
             properties.load(inputStream);
         } catch (FileNotFoundException e) {
