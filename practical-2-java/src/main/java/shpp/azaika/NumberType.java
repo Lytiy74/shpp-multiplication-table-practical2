59351bbb7d1454ab422ebd4fc8779689022a77af
Andrew Zaika <lytiy7404@gmail.com>
2024-11-30 16:07:54 +0200

    Removed Class<? extends Number> from enums

--- practical-2-java/src/main/java/shpp/azaika/NumberType.java
+++ practical-2-java/src/main/java/shpp/azaika/NumberType.java
@@ -1,17 +1,10 @@
 package shpp.azaika;
 
 public enum NumberType{
-    BYTE(Byte.class),
-    SHORT(Short.class),
-    INT(Integer.class),
-    LONG(Long.class),
-    FLOAT(Float.class),
-    DOUBLE(Double.class);
-    private final Class<? extends Number> typeClass;
-    NumberType(Class<? extends Number> typeClass) {
-        this.typeClass = typeClass;
-    }
-    public Class<? extends Number> getTypeClass() {
-        return typeClass;
-    }
+    BYTE,
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE;
 }
