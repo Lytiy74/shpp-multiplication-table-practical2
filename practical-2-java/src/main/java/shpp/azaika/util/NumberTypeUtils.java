b121f77b5ee77d743b6ec45bc1435e7fd3bbab77
Andrew Zaika <lytiy7404@gmail.com>
2024-12-01 10:30:08 +0200

    simplified convertToType and added logging

--- practical-2-java/src/main/java/shpp/azaika/util/NumberTypeUtils.java
+++ practical-2-java/src/main/java/shpp/azaika/util/NumberTypeUtils.java
@@ -1,13 +1,19 @@
 package shpp.azaika.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import shpp.azaika.NumberType;
 
 import static shpp.azaika.NumberType.SHORT;
 
 public class NumberTypeUtils {
-    private NumberTypeUtils() {}
+    private static final Logger logger = LoggerFactory.getLogger(NumberTypeUtils.class);
+    private NumberTypeUtils() {
+    }
+
     public static NumberType getSystemNumberType() {
         String property = System.getProperty("tableVariableType", "int").toLowerCase();
+        logger.debug("Retrieved tableVariableType from property: [{}]", property);
         return switch (property) {
             case "byte" -> NumberType.BYTE;
             case "short" -> SHORT;
@@ -20,14 +26,14 @@ public class NumberTypeUtils {
     }
 
     public static Number convertToType(Number value, NumberType numberType) {
+        logger.debug("Converting value type [{}] to type [{}]", value.getClass().getSimpleName(), numberType);
         return switch (numberType) {
-            case BYTE -> (byte) value.longValue();
-            case SHORT -> (short) value.longValue();
-            case INT -> (int) value.longValue();
+            case BYTE -> value.byteValue();
+            case SHORT -> value.shortValue();
+            case INT -> value.intValue();
             case LONG -> value.longValue();
             case FLOAT -> value.floatValue();
             case DOUBLE -> value.doubleValue();
-            default -> throw new IllegalArgumentException("Unsupported number type: " + numberType);
         };
     }
 }
