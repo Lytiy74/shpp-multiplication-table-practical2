14f18df6759178c12e465f8104b47dea5d1daf7a
Andrew Zaika <lytiy7404@gmail.com>
2024-12-01 10:29:10 +0200

    added default values for config and added logging

--- practical-2-java/src/main/java/shpp/azaika/util/Config.java
+++ practical-2-java/src/main/java/shpp/azaika/util/Config.java
@@ -1,22 +1,35 @@
 package shpp.azaika.util;
 
-import java.io.FileNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
     private static Properties properties;
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
+
+    private Config() {}
 
     static {
         try(InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties")) {
-            properties = new Properties();
+            Properties defaultProperties = initDefaultProperties();
+            properties = new Properties(defaultProperties);
             properties.load(inputStream);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error(e.getMessage(),e);
+            logger.warn("Unable to load config.properties, use default values ");
         }
+    }
+
+    private static Properties initDefaultProperties() {
+        Properties defaultProperties = new Properties();
+        defaultProperties.setProperty("table.calculation.minValue","1");
+        defaultProperties.setProperty("table.calculation.maxValue","10");
+        defaultProperties.setProperty("table.calculation.increment","1");
+        return defaultProperties;
     }
 
     public static String getProperty(String key) {
